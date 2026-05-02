090cfd4271d9c62c1d015ff7be6a4e0c3136d3b4
Geo-assist <gthuku057@gmail.com>
2026-05-02 14:04:25 +0300

    Updated UI, added delivery fee, fixed order flow, SMS integration, mobile responsiveness

--- src/Agrovet.Infrastructure/obj/Debug/net8.0/Agrovet.Infrastructure.AssemblyInfo.cs
+++ src/Agrovet.Infrastructure/obj/Debug/net8.0/Agrovet.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Agrovet.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96bba4a4fe17f6e0020d658acbacd884a0a5dd73")]
 [assembly: System.Reflection.AssemblyProductAttribute("Agrovet.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Agrovet.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
